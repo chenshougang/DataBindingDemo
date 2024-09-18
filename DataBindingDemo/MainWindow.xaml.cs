08a17af222e8a14d0869ef4a0d60940d78fbe2ef
chenshougang <1396748781@qq.com>
2024-09-18 17:23:54 +0800

    添加项目文件。

---
+++ DataBindingDemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBindingDemo
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 获取 DataContext 并将其强制转换为 PersonViewModel
+            var viewModel = (PersonViewModel)this.DataContext;
+            viewModel.Age += 1; // 每次点击按钮，年龄加 1
+
+            viewModel.Name = DateTime.Now.ToString();
+
+        }
+    }
+}
---
+++ DataBindingDemo/ViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindingDemo
+{
+    public class PersonViewModel : INotifyPropertyChanged
+    {
+        private Person _person;
+
+        public PersonViewModel()
+        {
+            _person = new Person { Name = "John Doe", Age = 30 };
+        }
+
+        public string Name
+        {
+            get { return _person.Name; }
+            set
+            {
+                _person.Name = value;
+                OnPropertyChanged("Name");
+
+                
+            }
+        }
+
+        public int Age
+        {
+            get { return _person.Age; }
+            set
+            {
+                _person.Age = value;
+                OnPropertyChanged("Age");
+
+               
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
